503a09226f98199e2a450701f3cad679136ee593
Randgrid-X <randgrid13@gmail.com>
2024-08-04 11:44:53 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+tasks_count = 12
+number_of_hours = 1.5
+name = 'Python'
+print('Курс:', name+',', 'всего задач:', str(tasks_count)+',', 'затрачено часов:', str(number_of_hours)+',', 'среднее время выполнения:', number_of_hours/tasks_count, 'часа')
